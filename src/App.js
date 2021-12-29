cdbf30640fedcdf23d56e0f2cb6f9f6ace8cf115
Henrique Medeiros <rique.medeirosneves@gmail.com>
2021-12-28 23:48:26 -0300

    Add calculator functions.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,73 @@
 import React, { useState } from 'react';
 import Botao from './components/botao';
+import * as math from 'mathjs';
 import './styles.css';
 
 const arrOper = ['*', '/', '+', '-', '.']
 
 function App() {
+  const [input, setInput] = useState("");
+
+  function insereNum(val){
+    setInput(input + val);
+  }
+
+  function insereOperacao(val){
+    if (input === "" || (arrOper.includes(input[input.length-1]) && arrOper.includes(val))) {
+      return;
+    }
+    else{
+      setInput(input + val);
+    }
+  }
+
+  function calc(){
+    if (input === "" || (arrOper.includes(input[input.length-1]))){
+      return input;
+    }
+    else{
+      setInput(math.evaluate(input))
+    }
+  }
+
   return (
     <div className='App'>
       <h1>Calculadora com React</h1>
       <div className='calc-wrapper'>
-        <Botao isInput></Botao>
+        <Botao isInput>{input}</Botao>
         <div className='linha'>
-          <Botao>CE</Botao>
-          <Botao>C</Botao>
+          <Botao onClick={() => setInput("")}>CE</Botao>
+          <Botao onClick={() => setInput(input.slice(0, -1))}>C</Botao>
           <Botao>%</Botao>
-          <Botao>/</Botao>
+          <Botao onClick={insereOperacao}>/</Botao>
         </div>
 
         <div className='linha'>
-          <Botao>7</Botao>
-          <Botao>8</Botao>
-          <Botao>9</Botao>
-          <Botao>x</Botao>
+          <Botao onClick={insereNum}>7</Botao>
+          <Botao onClick={insereNum}>8</Botao>
+          <Botao onClick={insereNum}>9</Botao>
+          <Botao onClick={insereOperacao}>*</Botao>
         </div>
 
         <div className='linha'>
-          <Botao>4</Botao>
-          <Botao>5</Botao>
-          <Botao>6</Botao>
-          <Botao>-</Botao>
+          <Botao onClick={insereNum}>4</Botao>
+          <Botao onClick={insereNum}>5</Botao>
+          <Botao onClick={insereNum}>6</Botao>
+          <Botao onClick={insereOperacao}>-</Botao>
         </div>
 
         <div className='linha'>
-          <Botao>1</Botao>
-          <Botao>2</Botao>
-          <Botao>3</Botao>
-          <Botao>+</Botao>
+          <Botao onClick={insereNum}>1</Botao>
+          <Botao onClick={insereNum}>2</Botao>
+          <Botao onClick={insereNum}>3</Botao>
+          <Botao onClick={insereOperacao}>+</Botao>
         </div>
 
         <div className='linha'>
           <Botao>+/-</Botao>
-          <Botao>0</Botao>
-          <Botao>.</Botao>
-          <Botao>=</Botao>
+          <Botao onClick={insereNum}>0</Botao>
+          <Botao onClick={insereOperacao}>.</Botao>
+          <Botao onClick={calc}>=</Botao>
         </div>
       </div>
     </div>
